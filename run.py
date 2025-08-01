399040848546366f4cd563c8d3d10d532ca136ea
Neha Singh <nehasingh1491@gmail.com>
2025-08-01 23:35:41 +0200

    done

--- run.py
+++ run.py
@@ -42,15 +42,15 @@ experiment1_concluded['t1InCorrNonMAGIX'] = 0
 experiment2_started = Counter()
 # experiment2_started['t2CorrMAGIX'] = 0
 # experiment2_started['t2CorrNonMAGIX'] = 0
-# experiment2_started['t2InCorrMAGIX'] = 0
-experiment2_started['t2InCorrNonMAGIX'] = 0
+experiment2_started['t2InCorrMAGIX'] = 0
+# experiment2_started['t2InCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have concluded
 experiment2_concluded = Counter()
 # experiment2_concluded['t2CorrMAGIX'] = 0
 # experiment2_concluded['t2CorrNonMAGIX'] = 0
-# experiment2_concluded['t2InCorrMAGIX'] = 0
-experiment2_concluded['t2InCorrNonMAGIX'] = 0
+experiment2_concluded['t2InCorrMAGIX'] = 0
+# experiment2_concluded['t2InCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have started
 experiment3_started = Counter()
@@ -68,17 +68,17 @@ experiment3_concluded['t3CorrMAGIX'] = 0
 
 # Counters of how many experiments have started
 experiment4_started = Counter()
-# experiment4_started['t4CorrMAGIX'] = 0
+experiment4_started['t4CorrMAGIX'] = 0
 # experiment4_started['t4CorrNonMAGIX'] = 0
 # experiment4_started['t4InCorrMAGIX'] = 0
-experiment4_started['t4InCorrNonMAGIX'] = 0
+# experiment4_started['t4InCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have concluded
 experiment4_concluded = Counter()
-# experiment4_concluded['t4CorrMAGIX'] = 0
+experiment4_concluded['t4CorrMAGIX'] = 0
 # experiment4_concluded['t4CorrNonMAGIX'] = 0
 # experiment4_concluded['t4InCorrMAGIX'] = 0
-experiment4_concluded['t4InCorrNonMAGIX'] = 0
+# experiment4_concluded['t4InCorrNonMAGIX'] = 0
 
 # Creation of log file based on id name
 html_tags = ["<li", "<ul", "<a"]

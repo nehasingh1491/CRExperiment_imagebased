09e7335735da111288c5ddceb9916db8ec5117ec
Neha Singh <nehasingh1491@gmail.com>
2025-09-05 21:23:15 +0200

    comment correct

--- run.py
+++ run.py
@@ -28,72 +28,72 @@ task2bis_path = os.path.join("resources", "experiments","task2bis")
 # Counters of how many experiments have started
 experiment1_started = Counter()
 # experiment1_started['t1CorrMAGIX'] = 0
-# experiment1_started['t1CorrNonMAGIX'] = 0
+experiment1_started['t1CorrNonMAGIX'] = 0
 # experiment1_started['t1InCorrMAGIX'] = 0
-experiment1_started['t1InCorrNonMAGIX'] = 0
+# experiment1_started['t1InCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have concluded
 experiment1_concluded = Counter()
 # experiment1_concluded['t1CorrMAGIX'] = 0
-# experiment1_concluded['t1CorrNonMAGIX'] = 0
+experiment1_concluded['t1CorrNonMAGIX'] = 0
 # experiment1_concluded['t1InCorrMAGIX'] = 0
-experiment1_concluded['t1InCorrNonMAGIX'] = 0
+# experiment1_concluded['t1InCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have started
 experiment2_started = Counter()
 # experiment2_started['t2CorrMAGIX'] = 0
 # experiment2_started['t2CorrNonMAGIX'] = 0
-# experiment2_started['t2InCorrMAGIX'] = 0
-experiment2_started['t2InCorrNonMAGIX'] = 0
+experiment2_started['t2InCorrMAGIX'] = 0
+# experiment2_started['t2InCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have concluded
 experiment2_concluded = Counter()
 # experiment2_concluded['t2CorrMAGIX'] = 0
 # experiment2_concluded['t2CorrNonMAGIX'] = 0
-# experiment2_concluded['t2InCorrMAGIX'] = 0
-experiment2_concluded['t2InCorrNonMAGIX'] = 0
+experiment2_concluded['t2InCorrMAGIX'] = 0
+# experiment2_concluded['t2InCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have started
 experiment3_started = Counter()
 # experiment3_started['t3CorrMAGIX'] = 0
 # experiment3_started['t3CorrNonMAGIX'] = 0
-# experiment3_started['t3InCorrMAGIX'] = 0
-experiment3_started['t3InCorrNonMAGIX'] = 0
+experiment3_started['t3InCorrMAGIX'] = 0
+# experiment3_started['t3InCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have concluded
 experiment3_concluded = Counter()
 # experiment3_concluded['t3CorrMAGIX'] = 0
 # experiment3_concluded['t3CorrNonMAGIX'] = 0
-# experiment3_concluded['t3InCorrMAGIX'] = 0
-experiment3_concluded['t3InCorrNonMAGIX'] = 0
+experiment3_concluded['t3InCorrMAGIX'] = 0
+# experiment3_concluded['t3InCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have started
 experiment4_started = Counter()
 # experiment4_started['t4CorrMAGIX'] = 0
-# experiment4_started['t4CorrNonMAGIX'] = 0
+experiment4_started['t4CorrNonMAGIX'] = 0
 # experiment4_started['t4InCorrMAGIX'] = 0
-experiment4_started['t4InCorrNonMAGIX'] = 0
+# experiment4_started['t4InCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have concluded
 experiment4_concluded = Counter()
 # experiment4_concluded['t4CorrMAGIX'] = 0
-# experiment4_concluded['t4CorrNonMAGIX'] = 0
+experiment4_concluded['t4CorrNonMAGIX'] = 0
 # experiment4_concluded['t4InCorrMAGIX'] = 0
-experiment4_concluded['t4InCorrNonMAGIX'] = 0
+# experiment4_concluded['t4InCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have started
 experiment2bis_started = Counter()
 # experiment2bis_started['t2bisCorrMAGIX'] = 0
 # experiment2bis_started['t2bisCorrNonMAGIX'] = 0
-# experiment2bis_started['t2bisInCorrMAGIX'] = 0
-experiment2bis_started['t2bisInCorrNonMAGIX'] = 0
+experiment2bis_started['t2bisInCorrMAGIX'] = 0
+# experiment2bis_started['t2bisInCorrNonMAGIX'] = 0
 
 # Counters of how many experiments have concluded
 experiment2bis_concluded = Counter()
 # experiment2bis_concluded['t2bisCorrMAGIX'] = 0
 # experiment2bis_concluded['t2bisCorrNonMAGIX'] = 0
-# experiment2bis_concluded['t2bisInCorrMAGIX'] = 0
-experiment2bis_concluded['t2bisInCorrNonMAGIX'] = 0
+experiment2bis_concluded['t2bisInCorrMAGIX'] = 0
+# experiment2bis_concluded['t2bisInCorrNonMAGIX'] = 0
 
 # Creation of log file based on id name
 html_tags = ["<li", "<ul", "<a"]
